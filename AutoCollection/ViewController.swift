564f5f3d22a54207519a18e126567c68dcd67856
StasBelousov <stasbelousov@icloud.com>
2020-09-18 10:01:30 +0300

    Initial Commit

---
+++ AutoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoCollection
+//
+//  Created by Станислав Белоусов on 18/09/2020.
+//  Copyright © 2020 Станислав Белоусов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
